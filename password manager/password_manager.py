269e93fe7d5ce8b56d83a1ac3640873b34c8e184
Zer0Gr@av!ty <140010217+zer0gra@users.noreply.github.com>
2023-07-28 19:13:50 +0200

    Update password_manager.py
    added a new button to update your passwords

--- password manager/password_manager.py
+++ password manager/password_manager.py
@@ -15,7 +15,7 @@ def load_key():
             key = key_file.read()
             return key
     except FileNotFoundError:
-        messagebox.showerror("Error", "No encryption key found. Please run the application with 'generate_key' parameter to create a key file.")
+        messagebox.showerror("Error", "No encryption key found. Please run the application in cmd with 'generate_key' parameter to create a key file.")
 
 def encrypt_message(message, key):
     cipher_suite = Fernet(key)
@@ -26,6 +26,38 @@ def decrypt_message(encrypted_message, key):
     cipher_suite = Fernet(key)
     decrypted_message = cipher_suite.decrypt(encrypted_message)
     return decrypted_message.decode()
+
+def update_password():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if website and username and password:
+        folder_path = os.path.join(os.getcwd(), 'passwords')
+        passwords_file = os.path.join(folder_path, 'passwords.json')
+
+        try:
+            with open(passwords_file, 'r') as file:
+                passwords = json.load(file)
+        except FileNotFoundError:
+            messagebox.showerror("Not Found", "Password file not found.")
+            return
+
+        key = load_key()
+
+        if website in passwords:
+            encrypted_username = encrypt_message(username, key)
+            encrypted_password = encrypt_message(password, key)
+            passwords[website]['username'] = encrypted_username.decode()
+            passwords[website]['password'] = encrypted_password.decode()
+            with open(passwords_file, 'w') as file:
+                json.dump(passwords, file)
+            messagebox.showinfo("Success", "Password updated successfully.")
+        else:
+            messagebox.showwarning("Not Found", "Password for this website not found.")
+    else:
+        messagebox.showwarning("Incomplete", "Please fill in all fields.")
+
 
 def save_password():
     website = website_entry.get()
@@ -112,25 +144,29 @@ else:
     password_entry = tk.Entry(window, font=("Arial", 12), show="*")
     password_entry.grid(row=2, column=1, padx=5, pady=5)
 
-    # Create buttons
-    save_button = tk.Button(window, text="Save Password", font=("Arial", 12, "bold"), command=save_password)
-    save_button.grid(row=3, column=0, padx=5, pady=5, sticky=tk.W)
+# Create buttons
+save_button = tk.Button(window, text="Save Password", font=("Arial", 12, "bold"), command=save_password)
+save_button.grid(row=3, column=0, padx=5, pady=5)
 
-    get_button = tk.Button(window, text="Get Password", font=("Arial", 12, "bold"), command=get_password)
-    get_button.grid(row=3, column=1, padx=5, pady=5, sticky=tk.E)
+get_button = tk.Button(window, text="Get Password", font=("Arial", 12, "bold"), command=get_password)
+get_button.grid(row=3, column=1, padx=5, pady=5)
 
-    # Configure grid weights for resizing
-    window.grid_columnconfigure(0, weight=1)
-    window.grid_columnconfigure(1, weight=1)
+update_button = tk.Button(window, text="Update Password", font=("Arial", 12, "bold"), command=update_password)
+update_button.grid(row=3, column=2, padx=5, pady=5)
 
-    # Set window size and position
-    window.geometry("400x200")  # Set the desired width and height
-    window.update_idletasks()  # Apply any pending geometry changes
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    x = (screen_width - window.winfo_width()) // 2
-    y = (screen_height - window.winfo_height()) // 2
-    window.geometry(f"+{x}+{y}")  # Center the window on the screen
+# Configure grid weights for resizing
+window.grid_columnconfigure(0, weight=1)
+window.grid_columnconfigure(1, weight=1)
+window.grid_columnconfigure(2, weight=1)
 
-    # Run the GUI event loop
-    window.mainloop()
+# Set window size and position
+window.geometry("500x250")  # Set the desired width and height
+window.update_idletasks()  # Apply any pending geometry changes
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+x = (screen_width - window.winfo_width()) // 2
+y = (screen_height - window.winfo_height()) // 2
+window.geometry(f"+{x}+{y}")  # Center the window on the screen
+
+# Run the GUI event loop
+window.mainloop()
